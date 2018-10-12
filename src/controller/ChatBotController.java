2f796e2179a6c3eaed8114c409ef225fce82dd4b
RyanPerkins <bobefett.qwert12@gmail.com>
2018-10-12 08:51:14 -0600

    built method skeletons

--- src/controller/ChatBotController.java
+++ src/controller/ChatBotController.java
@@ -1,8 +1,17 @@
 package controller;
 
+import model.ChatBot;
+
 public class ChatBotController
 {
+	private ChatBot myBot;
+	
 	public void start() {
 		
+	}
+	
+	public String interactWithChatbot(String botText) {
+		String something = "";
+		return something;
 	}
 }
--- src/model/ChatBot.java
+++ src/model/ChatBot.java
@@ -1,8 +1,27 @@
 package model;
 
+import java.util.ArrayList;
+
 public class ChatBot
 {
-	ChatBot(){
-		
+	private ArrayList<String> responseList;
+	private String currentUser;
+	private String joke;
+	private String content;
+	private ArrayList<String> spookyList;
+
+	public ChatBot()
+	{
+
+	}
+
+	public ChatBot(String currentUser)
+	{
+		this.currentUser = currentUser;
+	}
+	
+	public String askName() {
+		String name;
+		return name;
 	}
 }
