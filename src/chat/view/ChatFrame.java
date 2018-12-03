3149b34c572cf40a7e60bde50f2a9797d15f59ec
RyanPerkins <bobefett.qwert12@gmail.com>
2018-12-03 09:51:36 -0700

    completed the chatFrame class

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,8 +1,28 @@
 package chat.view;
 
 import javax.swing.JFrame;
+import chat.controller.ChatController;
 
 public class ChatFrame extends JFrame
 {
+	private ChatController appController;
+	private ChatPanel appPanel;
 	
+	public ChatFrame(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+		this.appPanel = new ChatPanel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(800, 800);
+		this.setTitle("Chat Bot");
+		this.setResizable(true);
+		this.setVisible(true);
+	}
 }
